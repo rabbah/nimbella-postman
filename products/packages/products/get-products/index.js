5b112099a24676e3868929271fbe54633c91763f
Rodric Rabbah <rodric@gmail.com>
2020-09-22 15:14:10 -0400

    Get products from db.

--- products/packages/products/get-products/index.js
+++ products/packages/products/get-products/index.js
@@ -1,95 +1,19 @@
+/* Pulls a list of all products. */
+const nimbella = require('nim')
+const kv = nimbella.redis()
 
-/* Pulls a list of all product */
+async function main(params) {
+    const products = await kv
+        .keysAsync('/*')
+        .then(ids => ids.map(async id => kv.getAsync(id).then(JSON.parse)))
 
-function main(params) {
     return {
         statusCode: 200,
         headers: {
             "Content-Type": "application/json",
         },
-        body: [
-            {
-                "identifier": "product-three",
-                "name": "Product Three",
-                "description": "This is a description of product three, providing the details of this product.",
-                "image": "http://example.com/image.jpg",
-                "url": "http://example.com",
-                "brand": "Union Fashion",
-                "category": "Test",
-                "color": "Blue",
-                "logo": "http://example.com/image.jpg",
-                "manufacturer": "Unon Fashion",
-                "material": "Cotton",
-                "model": "Test",
-                "releaseDate": "01/01/2020",
-                "sku": "test-1",
-                "width": "0",
-                "weight": "0",
-                "depth": "0",
-                "height": "0"
-            },
-            {
-                "identifier": "product-two",
-                "name": "Product Two",
-                "description": "This is a description of product two-two-two, providing the details of this product.",
-                "image": "http://example.com/image.jpg",
-                "url": "http://example.com",
-                "brand": "Union Fashion",
-                "category": "Test",
-                "color": "Blue",
-                "logo": "http://example.com/image.jpg",
-                "manufacturer": "Unon Fashion",
-                "material": "Cotton",
-                "model": "Test",
-                "releaseDate": "01/01/2020",
-                "sku": "test-1",
-                "width": "0",
-                "weight": "0",
-                "depth": "0",
-                "height": "0"
-            },
-            {
-                "identifier": "product-one",
-                "name": "Product One",
-                "description": "This is a description of product one, providing the details of this product.",
-                "image": "http://example.com/image.jpg",
-                "url": "http://example.com",
-                "brand": "Union Fashion",
-                "category": "Test",
-                "color": "Blue",
-                "logo": "http://example.com/image.jpg",
-                "manufacturer": "Unon Fashion",
-                "material": "Cotton",
-                "model": "Test",
-                "releaseDate": "01/01/2020",
-                "sku": "test-1",
-                "width": "0",
-                "weight": "0",
-                "depth": "0",
-                "height": "0"
-            },
-            {
-                "identifier": "product-four",
-                "name": "Product Four",
-                "description": "This is a description of product four, providing the details of this product.",
-                "image": "http://example.com/image.jpg",
-                "url": "http://example.com",
-                "brand": "Union Fashion",
-                "category": "Test",
-                "color": "Blue",
-                "logo": "http://example.com/image.jpg",
-                "manufacturer": "Unon Fashion",
-                "material": "Cotton",
-                "model": "Test",
-                "releaseDate": "01/01/2020",
-                "sku": "test-1",
-                "width": "0",
-                "weight": "0",
-                "depth": "0",
-                "height": "0"
-            }
-        ],
-    };
+        body: await Promise.all(products)
+    }
 }
 
-exports.main = main;
+exports.main = main
