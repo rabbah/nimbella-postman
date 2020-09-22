a1e356d99e4290bc8d7093b2a591cd2dd89444c5
Rodric Rabbah <rodric@gmail.com>
2020-09-22 15:46:37 -0400

    Add product to db.

--- products/packages/products/add-product/index.js
+++ products/packages/products/add-product/index.js
@@ -1,16 +1,41 @@
-
-/* Creates a new productz. */
+/* Creates a new products. */
+const nimbella = require('nim')
+const kv = nimbella.redis()
+const uuid = require('uuid')
 
 function main(params) {
-    return {
-        statusCode: 201,
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: {
-            "status": "success"
-        },
-    };
+    const product = {
+        id: `/${uuid.v4()}`,
+        category: params.category,
+        brand: params.brand,
+        color: params.color,
+        image: params.image,
+        unitPrice: params.unitPrice
+    }
+    
+    return kv
+        .setAsync(product.id, JSON.stringify(product))
+        .then(_ => ({
+            statusCode: 201,
+            headers: {
+                "Content-Type":"application/json",
+            },
+            body: {
+                status: "success"
+            }
+        }))
+        .catch(error => {
+            console.log(error)
+            return {
+                statusCode: 400,
+                headers: {
+                    "Content-Type":"application/json",
+                },
+                body: {
+                    status: "Oops!"
+                }
+            }
+        })
 }
 
-exports.main = main;
+exports.main = main
